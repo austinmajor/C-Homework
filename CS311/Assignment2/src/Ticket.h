2c94cdd7ab1dc7e25c6f1ebfcf61c5db7544d58a
Austin Major <mr.austinmajor@gmail.com>
2018-01-25 19:54:13 -0600

    asldkfj

---
+++ CS311/Assignment2/src/Ticket.h
@@ -0,0 +1,38 @@
+/*
+File Name: Ticket.h
+Author: Austin Major
+Student ID: N675Q967
+Assignment Number: 1
+*/
+
+//This is where Function Declarations go
+#include <iostream>
+#include <string>
+
+class Ticket{
+  public:
+  //setters
+  void setTicketNumber(std::string input);
+  void setGrossWeight(double input);
+  void setTareWeight(double input);
+  void setMoistureLevel(double input);
+  void setForeignMaterial(double input);
+
+  //getters
+  std::string getTicketNumber() const;
+  double getGrossWeight() const;
+  double getTareWeight() const;
+  double getMoistureLevel() const;
+  double getForeignMaterial() const;
+
+  //calculation
+  double calculateNetWeight();
+  double calculateGrossBushels();
+  double calculateMoistureDockage();
+  double calculateForeignMaterialDockage();
+  double calculateNetBushels();
+
+  private:
+  std::string ticketNumber;
+  double grossWeight, tareWeight, moistureLevel, foreignMaterial;
+};
